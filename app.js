754579dd825ce3a440011a250f35b6f9a8c75d6c
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-10 18:53:54 +0600

    prototype explored

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ function User(email, name){
     this.online = false;   
 }
 
-
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, 'has logged in');
+}
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.email, 'has logged  out');
+}
 
 
 let userOne = new User('sam@offgrid.com', 'Sam');
 let userTwo = new User('shimul@offgrid.com', 'Shimul');
 
 console.log(userOne);
+userTwo.login();
+userOne.logout();
